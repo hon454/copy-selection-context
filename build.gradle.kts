47a00b5ddeeb098cb672a250421c6bd3bdd13ef1
Jihun Jeon <hon454@gmail.com>
2026-02-17 01:23:11 +0900

    chore(scaffold): initialize Gradle project with IntelliJ Platform Plugin 2.11.0
    Set up Gradle 9.3.1 build with IntelliJ Platform Plugin 2.11.0, JVM
    toolchain 21, and plugin.xml skeleton targeting IDE build 243+. Include
    gradle wrapper, .gitignore, and settings.gradle.kts.

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.11.0"
+}
+
+group = "com.github.hon45"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.3")
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild.set("243")
+        }
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "21"
+    }
+}
