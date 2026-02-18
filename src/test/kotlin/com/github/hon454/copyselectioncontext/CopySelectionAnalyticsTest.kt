176c1be79b1e3c1fbf9d2953f0e40a21e9c6da78
Jihun Jeon <hon454@gmail.com>
2026-02-18 09:38:54 +0900

    fix: correct JUnit imports in CopySelectionAnalyticsTest to use JUnit 5
    Replace JUnit 4 assertion imports (org.junit.Assert) with JUnit 5
    (org.junit.jupiter.api.Assertions) to match the project test framework.

--- src/test/kotlin/com/github/hon454/copyselectioncontext/CopySelectionAnalyticsTest.kt
+++ src/test/kotlin/com/github/hon454/copyselectioncontext/CopySelectionAnalyticsTest.kt
@@ -1,7 +1,7 @@
 package com.github.hon454.copyselectioncontext
 
-import org.junit.Test
-import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class CopySelectionAnalyticsTest {
 
